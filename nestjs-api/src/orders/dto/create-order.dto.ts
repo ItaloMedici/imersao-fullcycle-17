14d2c2b2372f6a349b52f973e98a0a4a3b4831d6
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-01-30 21:36:29 -0300

    feat: add order crud and entities

--- nestjs-api/src/orders/dto/create-order.dto.ts
+++ nestjs-api/src/orders/dto/create-order.dto.ts
@@ -1,1 +1,33 @@
-export class CreateOrderDto {}
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+import { OrderItem } from '../entities/order-item.entity';
+
+export class CreateOrderDto {
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItem)
+  items: OrderItemsDtop[];
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  card_hash: string;
+}
+
+export class OrderItemsDtop {
+  @IsString()
+  @IsNotEmpty()
+  product_id: string;
+
+  @MaxLength(36)
+  @IsInt()
+  @IsNotEmpty()
+  quantity: number;
+}
--- nestjs-api/src/orders/orders.service.ts
+++ nestjs-api/src/orders/orders.service.ts
@@ -1,26 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { In, Repository } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createOrderDto: CreateOrderDto) {
+    const unicProductsIds = [
+      ...new Set(createOrderDto.items.map((item) => item.product_id)),
+    ];
+
+    const products = await this.productRepository.findBy({
+      id: In(unicProductsIds),
+    });
+
+    if (products.length !== unicProductsIds.length) {
+      throw new Error(
+        `Alguns produtos não existem! Produtos encontrados: ${unicProductsIds} esperado: ${unicProductsIds.length}`,
+      );
+    }
+
+    const order = Order.create({
+      client_id: 1,
+      items: createOrderDto.items.map((item) => {
+        const product = products.find(
+          (product) => product.id === item.product_id,
+        );
+        return {
+          price: product.price,
+          product_id: item.product_id,
+          quantity: item.quantity,
+        };
+      }),
+    });
+
+    return await this.orderRepository.save(order);
   }
 
   findAll() {
-    return `This action returns all orders`;
+    return this.orderRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
-  }
-
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  findOne(id: string) {
+    return this.orderRepository.findOne({ where: { id } });
   }
 }
